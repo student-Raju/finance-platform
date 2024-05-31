56f894f81911f3219e34e0778e31efd303b7e955
student-Raju <rajgopalmahata@gmail.com>
2024-05-31 11:44:14 +0530

    finance

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,6 @@
 import { SignIn,ClerkLoaded,ClerkLoading } from "@clerk/nextjs";
+import Image from "next/image";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
   return (
@@ -11,9 +13,17 @@ export default function Page() {
               </p>
         </div>
         <div className="flex items-center justify-center mt-8">
+        <ClerkLoaded>
         <SignIn path="/sign-in" />
+        </ClerkLoaded>
+        <ClerkLoading>
+          <Loader2 className="animate-spin text-muted-foreground"/>
+        </ClerkLoading>
         </div>
       
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+              <Image src="/logo.svg" height={100} width={100} alt="Logo"/>
       </div>
     
      </div>
