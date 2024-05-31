cfa296d12e474a8841377fd2b0474211f0be19af
student-Raju <rajgopalmahata@gmail.com>
2024-05-31 17:45:59 +0530

    finance-platform

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,39 +3,16 @@ import { z } from 'zod';
 import { zValidator } from '@hono/zod-validator';
 import { handle } from 'hono/vercel';
 
+
+
+
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app
-.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
-  })
+app.get("/hello",(c)=>{
+  return c.json({hello:"world"})
 })
-.get(
-  "/hello/:test",
-(c)=>{
-  return c.json({
-    message:"Hello World",
-    
-  })
- })
- .post("/create/:postId",
-  zValidator("json",z.object({
-    name:z.string(),
-    userId:z.number(),
-  })),
-   zValidator("param",z.object({
-   
-    postId:z.number(),
-  })),
-(c)=>{
-  const{name,userId}=c.req.valid("json");
-   const{postId}=c.req.valid("param");
-  return c.json({});
-})
-
 
 export const GET = handle(app);
 export const POST = handle(app);
