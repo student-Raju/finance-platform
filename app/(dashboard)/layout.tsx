283c2cea0b71802fdf7b1ff3463532178d4bd15c
student-Raju <rajgopalmahata@gmail.com>
2024-05-31 21:13:14 +0530

    finance

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import { Header } from "@/components/header";
+type props={
+    children:React.ReactNode;
+}
+
+const DashboardLayout = ({children}:props) => {
+  return (
+    <>
+    <Header/>
+        <main className="px-3 lg:px-14">
+        {children}
+        </main>
+    </>
+  )
+}
+
+export default DashboardLayout;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <UserButton signInUrl="/"/>
+    <div>
+      Dashboard page
+    </div>
   );
 }
