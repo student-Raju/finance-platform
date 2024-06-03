5fbedb94110957b9c910443130b68b1fed9c5116
student-Raju <rajgopalmahata@gmail.com>
2024-06-03 16:39:11 +0530

    Finance-Platform

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,34 +1,42 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import {Card,
     CardContent,
     CardHeader,
     CardTitle
 } from "@/components/ui/card";
 
-import { Plus } from "lucide-react";
-import { Payment, columns } from "./columns";
+import { Loader2, Plus } from "lucide-react";
+import {  columns } from "./columns";
 import { DataTable } from "@/components/data-table";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
-  const data: Payment[]= [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },  
-    {
-        id: "728ed52f",
-        amount: 50,
-        status: "success",
-        email: "a@example.com",
-      },  
-  ]
+
+  
 
 const AccountsPage = () => {
     const newAccount=useNewAccount();
+    const accountQuery=useGetAccounts();
+    const accounts= accountQuery.data || [];
+    if(accountQuery.isLoading){
+      return(
+        <div  className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+            <CardHeader>
+              <Skeleton className="h-8 w-48"/>
+              </CardHeader>
+              <CardContent>
+                <div className="h-[500px] w-full flex items-center justify-center">
+                  <Loader2 className="size-6 text-slate-300 animate-spin"/>
+                </div>
+              </CardContent>
+            </Card>
+        </div>
+      );
+    }
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
         <Card className="border-none drop-shadow-sm">
@@ -42,11 +50,10 @@ const AccountsPage = () => {
       </Button>
 
        </CardHeader>
-       <CardContent>
-        
+       <CardContent>  
       <DataTable
       filterKey="email"
-       columns={columns} data={data} onDelete={()=>{}}
+       columns={columns} data={accounts} onDelete={()=>{}}
        disabled={false}/>
        </CardContent>
         </Card>
