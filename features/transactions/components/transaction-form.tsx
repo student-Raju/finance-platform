b139c37333a216ae556d01c96af528d6ebc1aa62
student-Raju <rajgopalmahata@gmail.com>
2024-06-06 10:09:46 +0530

    Transctions Page

--- features/transactions/components/transaction-form.tsx
+++ features/transactions/components/transaction-form.tsx
@@ -6,6 +6,7 @@ import { Input } from "@/components/ui/input";
 import  {Button} from "@/components/ui/button";
 import { insertTranasctionSchema } from "@/db/schema";
 import { DatePicker } from "@/components/date-picker";
+import { AmountInput } from "@/components/amount-input";
 import{
 Form,
 FormControl,
@@ -15,6 +16,8 @@ FormLabel,
 FormMessage,
 } from "@/components/ui/form";
 import { Select } from "@/components/select";
+import { Textarea } from "@/components/ui/textarea";
+import { convertAmountToMiliunits } from "@/lib/utils";
 
 const formSchema=z.object({
  date:z.coerce.date(),
@@ -57,9 +60,15 @@ export const TransactionForm=({
         defaultValues:defaultValues,
     });
     const handleSubmit=(values: FormValues)=>{
-       console.log({values});
+      const amount=parseFloat(values.amount)
+      const amountInMiliunits= convertAmountToMiliunits(amount);
+      
+      onSubmit({
+        ...values,
+        amount:amountInMiliunits,
+      });
         
-    }
+    };
     const handleDelete=()=>{
         onDelete?.();
     }
@@ -118,9 +127,61 @@ return(
                    disabled={disabled}/>
                 </FormControl>
               </FormItem>
+          )}
+          />
+                <FormField
+          name="payee"
+          control={form.control}
+          render={({field})=>(
+              <FormItem>
+                <FormLabel>
+                    payee
+                </FormLabel>
+                <FormControl>
+                  <Input
+                  disabled={disabled}
+                  placeholder="Add a payee"
+                  {...field}/>
+                </FormControl>
+              </FormItem>
+          )}/>
+           <FormField
+          name="amount"
+          control={form.control}
+          render={({field})=>(
+              <FormItem>
+                <FormLabel>
+                    Amount
+                </FormLabel>
+                <FormControl>
+                  <AmountInput
+                  {...field}
+                  disabled={disabled}
+                  placeholder="0.00"
+                  />
+                </FormControl>
+              </FormItem>
+          )}/>
+          <FormField
+          name="notes"
+          control={form.control}
+          render={({field})=>(
+              <FormItem>
+                <FormLabel>
+                    Notes
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                  {...field}
+                  value={field.value ?? ""}
+                  disabled={disabled}
+                  placeholder="Optional notes"
+                  />
+                </FormControl>
+              </FormItem>
           )}/>
           <Button className="w-full" disabled={disabled}>
-            {id? "Save changes":"Create account"}
+            {id? "Save changes":"Create transaction"}
           </Button>
           {!!id&&(<Button
           type="button"
@@ -129,10 +190,10 @@ return(
           className="w-full"
           variant="outline">
             <Trash className="size-4 mr-2"/>
-            Delete account
+            Delete transaction
           </Button>)}
      </form>
     </Form>
-)
+);
 };
 
