8944074840893acbe175dc685ff8e3fd9121d6fa
student-Raju <rajgopalmahata@gmail.com>
2024-06-14 22:52:25 +0530

    Project Completed

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import {clerkMiddleware,getAuth} from '@hono/clerk-auth';
 import  accounts  from "./accounts";
 import categories from "./categories";
 import  transactions  from "./transactions";
@@ -9,9 +10,11 @@ import summary from "./summary";
 
 
 
-export const runtime = "edge";
+export const runtime = 'nodejs';
 
 const app = new Hono().basePath("/api");
+
+app.use('*', clerkMiddleware());
 
 
 const routes = app
