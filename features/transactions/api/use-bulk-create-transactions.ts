f02c52ef8a059b34081753894f553e4001e78904
student-Raju <rajgopalmahata@gmail.com>
2024-06-14 17:39:44 +0530

    Date and Account Filters

--- features/transactions/api/use-bulk-create-transactions.ts
+++ features/transactions/api/use-bulk-create-transactions.ts
@@ -21,6 +21,7 @@ export const useBulkCreateTransactions=()=>{
         onSuccess: ()=>{
             toast.success("Transactions created");
              queryClient.invalidateQueries({queryKey:["transactions"]});
+             queryClient.invalidateQueries({queryKey:["summary"]});
         },
         onError: ()=>{
             toast.error("Failed to create transactions");
